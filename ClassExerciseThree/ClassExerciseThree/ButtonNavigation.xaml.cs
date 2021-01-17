30b7fbe6996b181857e3dc83a95398c491c05af8
CMRoss <ChrisRoss@u.boisestate.edu>
2021-01-16 17:55:38 -0700

    Add project files.

---
+++ ClassExerciseThree/ClassExerciseThree/ButtonNavigation.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using InClass2;
+
+namespace ClassExerciseThree
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonNavigation : ContentPage
+    {
+        public ButtonNavigation()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LabelDemo());
+        }
+
+        private void BoxView_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BoxViewDemo());
+        }
+
+        private void ScrollView_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ScrollViewDemo());
+        }
+    }
+}
